f7e5cad9a24dc1673ce4704fb1bbc29c8dc214e4
hugolh <75566783+hugolh@users.noreply.github.com>
2024-09-17 16:38:00 +0200

    update folder

---
+++ src/main/java/com/pokemon/IPokemonMetadataProvider.java
@@ -0,0 +1,21 @@
+package com.pokemon;
+
+/**
+ * An IPokemonMetadataProvider aims to provide PokemonMetadata
+ * for a given pokemon index.
+ * 
+ * @author fv
+ */
+public interface IPokemonMetadataProvider {
+
+	/**
+	 * Retrieves and returns the metadata for the pokemon
+	 * denoted by the given <tt>index</tt>.
+	 * 
+	 * @param index Index of the pokemon to retrieve metadata for.
+	 * @return Metadata of the pokemon.
+	 * @throws PokedexException If the given <tt>index</tt> is not valid.
+	 */
+	PokemonMetadata getPokemonMetadata(int index) throws PokedexException;
+	
+}

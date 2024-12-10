11716b03c3496b59245dbef8022bb35999542660
hugolh <75566783+hugolh@users.noreply.github.com>
2024-12-10 16:45:39 +0100

    update implementation

--- src/main/java/com/pokemon/PokemonFactory.java
+++ src/main/java/com/pokemon/PokemonFactory.java
@@ -1,8 +1,40 @@
 package com.pokemon;
 
 public class PokemonFactory implements IPokemonFactory {
+
+    private final IPokemonMetadataProvider metadataProvider;
+
+    /**
+     * Constructor that takes a metadata provider.
+     *
+     * @param metadataProvider Provider of Pokemon metadata.
+     */
+    public PokemonFactory(IPokemonMetadataProvider metadataProvider) {
+        this.metadataProvider = metadataProvider;
+    }
+
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-        return new Pokemon(index, "PokemonName", 100, 100, 100, cp, hp, dust, candy, 50);
+        try {
+            PokemonMetadata metadata = metadataProvider.getPokemonMetadata(index);
+
+            int maxStats = metadata.getAttack() + metadata.getDefense() + metadata.getStamina();
+            double ivPercentage = ((15.0 + 15.0 + 15.0) / maxStats) * 100;
+
+            return new Pokemon(
+                index,
+                metadata.getName(),
+                metadata.getAttack(),
+                metadata.getDefense(),
+                metadata.getStamina(),
+                cp,
+                hp,
+                dust,
+                candy,
+                ivPercentage
+            );
+        } catch (PokedexException e) {
+            throw new IllegalArgumentException("Invalid Pokemon index: " + index, e);
+        }
     }
 }
